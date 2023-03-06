e3c69312aafb457755b1b8ca669d7156a99d120f
Ometere <ookuoluse@yahoo.com>
2023-03-06 23:20:12 +0100

    Second commit

--- index.js
+++ index.js
@@ -24,15 +24,23 @@ app.use(cors ({
 
 app.use('/auth', authRoute);
 
-app.use(cookieSession
-    (
-       {
-        name: 'session',
-        keys: ['ometere'],
-        maxAge: 24*60*60*100,
-       } 
-));
+// app.use(cookieSession
+//     (
+//        {
+//         name: 'session',
+//         keys: ['ometere'],
+//         maxAge: 24*60*60*100,
+//        } 
+// ));
+app.get("/", (req, res) => { 
+    res.send({
+      Status: "Running",
+      Message: "Hello from social Media Login",
+    });
+  })
+const PORT = process.env.PORT|| 5000;
 
-app.listen('5000', () => {
-    console.log('Server is running')
-});
+app.listen(PORT, () => {
+    console.log(`Server Running on: http://localhost:${PORT}`)
+   
+  });
